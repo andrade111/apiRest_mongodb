3f39b8e2d1b6e6951b2c4acb6c28fc2e55f18a71
Gabriel Andrade <andradeoliveirabiel@gmail.com>
2026-05-25 10:17:19 -0300

    collecting user by Id

--- src/main/java/com/andrade111/workshop_mongodb/services/UserService.java
+++ src/main/java/com/andrade111/workshop_mongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.andrade111.workshop_mongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andrade111.workshop_mongodb.domain.User;
 import com.andrade111.workshop_mongodb.repository.UserRepository;
+import com.andrade111.workshop_mongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
 }

46fa329fabe15bd5eba8de0a083c217190d72d2d
Gabriel Andrade <andradeoliveirabiel@gmail.com>
2026-05-27 12:58:43 -0300

    query methods

--- src/main/java/com/andrade111/workshop_mongodb/repository/PostRepository.java
+++ src/main/java/com/andrade111/workshop_mongodb/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.andrade111.workshop_mongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import com.andrade111.workshop_mongodb.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
-	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 }

5567ea47e3b2aa5d8faf2f22e5a952c3eb87e754
Gabriel Andrade <andradeoliveirabiel@gmail.com>
2026-05-27 11:34:58 -0300

    retrieving a post by id

---
+++ src/main/java/com/andrade111/workshop_mongodb/services/PostService.java
@@ -0,0 +1,23 @@
+package com.andrade111.workshop_mongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andrade111.workshop_mongodb.domain.Post;
+import com.andrade111.workshop_mongodb.repository.PostRepository;
+import com.andrade111.workshop_mongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado."));
+	}
+	
+}
